e1cf1a416a19379e4b700621f4c5208fe1cbf8f3
Tosha_Rotten <kozlovad@mer.ci.nsu.ru>
2022-11-15 23:50:22 +0700

    added discription of kata

--- sum/main.c
+++ sum/main.c
@@ -1,6 +1,11 @@
 #include <stddef.h>
 #include <stdio.h>
 
+//Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers.
+// No floats or non-positive integers will be passed.
+
+//For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
 long sum_two_smallest_numbers(size_t n, const int numbers[n]) {
     long int max = numbers[0];
     for (int i = 0; i < (int)n; i++){if (max < numbers[i]){max = numbers[i];}}
